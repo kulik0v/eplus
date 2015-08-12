e666da30c154b1a2005304d10fc84ec4a1876832
Alexander Kulikov <kulikov@wix.com>
2015-08-12 19:30:41 +0300

    init

---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- encoding: UTF-8 -*-
+
+import os
+from setuptools import setup, find_packages
+
+# install_requires = [
+# ]
+# dependency_links = [
+#     'http://dist.repoze.org/',
+#     'http://pypi.pinaxproject.com/',
+# ]
+
+setup(
+    name="eplus",
+    version="0.1",
+    description="ga Engine Plus",
+    author="WIX",
+
+    # packages=find_packages(),
+    packages=['eplus'],
+    # install_requires=[],
+)
+
+# rm -rf dist build eplus.egg-info; python setup.py bdist_egg
