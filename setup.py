96815b952b260ae98877e722d25957077acc9dfd
Alexander Kulikov <kulikov@wix.com>
2015-08-13 12:55:48 +0300

    setup up

--- setup.py
+++ setup.py
@@ -3,22 +3,38 @@
 import os
 from setuptools import setup, find_packages
 
-# install_requires = [
-# ]
-# dependency_links = [
-#     'http://dist.repoze.org/',
-#     'http://pypi.pinaxproject.com/',
-# ]
+
+
+# here = path.abspath(path.dirname(__file__))
+# with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+#     long_description = f.read()
+
 
 setup(
-    name="eplus",
-    version="1.0",
-    description="Google App Engine plus",
-    author="WIX",
+    name='eplus',
+    version='1.0.0',
+    description='Google App Engine extensions',
+    # long_description=long_description,
+    url='https://github.com/kulik0v/eplus',
+    author='WIX',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+    ],
 
     # packages=find_packages(),
     packages=['eplus'],
     # install_requires=[],
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    # entry_points={
+    #     'console_scripts': [
+    #         'sample=sample:main',
+    #     ],
+    # },
 )
 
-# python setup.py bdist_egg
