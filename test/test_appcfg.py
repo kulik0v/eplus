bb3d43f6064889c62445f4b8562b14804b3eb5fa
kaa <kulikov@gmail.com>
2019-09-12 15:38:49 +0300

    appcfg_update some basic tests

--- test/test_appcfg.py
+++ test/test_appcfg.py
@@ -7,7 +7,7 @@ from eplus.appcfg_update import get_unniq_target_yaml
 
 class TestAppcfg(unittest.TestCase):
 
-    def test_shutterstock_image(self):
+    def test_target_yaml(self):
         fn = get_unniq_target_yaml('app.yaml')
         self.assertFalse(os.path.isfile(fn))
 
