cceaf535f0f594e867e3e89cbcfd9b12669c00cf
kaa <kulikov@gmail.com>
2019-09-12 16:45:00 +0300

    appcfg_update some tests

---
+++ test/fake.py
@@ -0,0 +1,9 @@
+# -*- encoding: UTF-8 -*-
+import sys
+
+argv = []
+
+
+def main():
+    global argv
+    argv = sys.argv
--- test/test_appcfg.py
+++ test/test_appcfg.py
@@ -1,18 +1,43 @@
 # -*- encoding: UTF-8 -*-
 import os
+import sys
 import unittest
 
-from eplus.appcfg_update import get_unniq_target_yaml
+from eplus.appcfg_update import get_unniq_target_yaml, simulate_legacy_update
+from eplus.environment import init, init_lib
 
 
 class TestAppcfg(unittest.TestCase):
 
+    def setUp(self):
+        init()
+        init_lib()
+
+        # noinspection PyUnresolvedReferences,PyPackageRequirements
+        import gcloud
+        import fake
+        gcloud._import_gcloud_main = lambda *args: fake
+        sys.exit = lambda *args: None
+
+
+    def test_main_is_callable(self):
+        # noinspection PyUnresolvedReferences,PyPackageRequirements
+        from gcloud import main
+        main()
+
+
+    def test_basic_e2e(self):
+
+        sys.argv = ['appcfg_update', 'test.yaml']
+        simulate_legacy_update()
+
+        from fake import argv
+        self.assertIn('gcloud', argv)
+        self.assertEqual(['gcloud', 'app', 'deploy'], argv[:3])
+        self.assertIn('--project=move_it_to_args', argv)
+
+
     def test_target_yaml(self):
         fn = get_unniq_target_yaml('app.yaml')
         self.assertFalse(os.path.isfile(fn))
-
-
-
-
-
 
